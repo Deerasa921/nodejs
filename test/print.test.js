eeb2a8430bb87a343dfefbe4eca21c2b2db25e64
Deerasa921 <yeligetydheerasa921@gmail.com>
2023-03-21 08:47:38 +0530

    added files

---
+++ test/print.test.js
@@ -0,0 +1,7 @@
+const print = require('../src/print')
+
+test('prints "hello world"', () => {
+    const consoleSpy = jest.spyOn(global.console, 'log')
+    print('hello world')
+    expect(consoleSpy).toHaveBeenCalledWith('hello world')
+});
