394c7ee4fe16239ad07f14cb96d1f75978ed9d9b
Deerasa921 <yeligetydheerasa921@gmail.com>
2023-03-23 09:50:10 +0530

    changes in config1

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // other Jest configuration options...
     reporters: ['default', 'jest-junit'],
-    testResultsProcessor: 'jest-junit',
+   
     junitReporter: {
       outputDirectory: './test-results',
       outputName: 'junit.xml',
